42eb2c3fd0d356cb51f0246141fe9b853c8ec9d4
erlendoeien <pappegoyen1@gmail.com>
2020-11-24 20:25:53 +0100

    Init basic typescript config

--- frontend challenge/src/App.tsx
+++ frontend challenge/src/App.tsx
@@ -5,9 +5,7 @@ import Typography from "@material-ui/core/Typography";
 function App() {
   return (
     <Container maxWidth="md">
-      <Typography variant="h1">
-        Icon cards
-      </Typography>
+      <Typography variant="h1">Icon cards</Typography>
       Edit <code>src/App.js</code> and save to reload.
     </Container>
   );
--- frontend challenge/src/index.tsx
+++ frontend challenge/src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import CssBaseline from "@material-ui/core/CssBaseline"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
